55f86b3f352d9583e8fca4e048b166c28b5338fb
Eamon <eamon@piratecat.com>
2020-04-23 23:42:46 -0400

    Change transfer func based on connor's help

--- first-sim.m
+++ first-sim.m
@@ -1,15 +1,23 @@
 clear all
 close all
 
-Z = 400;
-num = Z;
+mu = 1;
+q1 = 1;
+N = 10;
+A = 0.001;
+R = 0.1;
+L = 1;
+g = -9.81;
 
-sys = tf([num],[1,0,0])
+b = 5;
+a = sqrt((5*mu*q1*N*A)/(4*pi*R*L*g))
 
-con = tf([20,1],[1])
+Z = (mu*q1*N*A)/(4*pi*R*L)
+
+sys = tf([1 0 z*b/a^3],[z/a^2])
 
 figure
-step(sys*con)
+step(sys)
 
 % step(sys)
 
