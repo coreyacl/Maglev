ef9a03a47c94e1ec6e5242a7ac36c35d5d12bd0f
coreyacl <corey.cochranlepiz@gmail.com>
2020-05-07 13:50:24 -0600

    added zesty zero

--- firstSim.m
+++ firstSim.m
@@ -18,7 +18,7 @@ L = 30e-3;
 %First we chose our 'b' setpoint for voltage
 %And find an a that cancels out the constant
 b = 5;
-a = abs(sqrt((b*mu*q1*N*A)/(4*pi*R*g)));
+a = abs(sqrt((b*mu*q1*N*A)/(4*pi*g)));
 
 %Z allows us to use cleaner math by placing
 %all of our constants into one constant.
@@ -29,34 +29,39 @@ G = tf([Z/a^2],[1 0 Z*b/a^3]);
 %% Closed Loop
 close all
 
-k = 2;
-kd = 1/2;
-kp = 50;
-ki = 2500;
+k = 40;
+kd = 1;
+kp = 200;
+ki = 10000;
 H = 1;
 Ac = tf([1],[L R])
 C = tf([kd,kp,ki],[1,0])
 C = C*k;
 M = 0.03;
 
+nu = tf([1,200],[1])
+
 % Visualize system
 % open loop
-Gol = C*G*H*Ac;
+Gol = C*G*H*Ac*nu;
 
 % close loop
-CL = (C*G*Ac)/(1+C*G*H*Ac) - 0.03;
+CL = (C*G*Ac*nu- 0.03)/(1+C*G*H*Ac*nu- 0.03) ;
 
-%figure
-%subplot(2,1,1)
-%pzmap(CL)
-%subplot(2,1,2)
-%rlocus(Gol)
-
-%figure
-%step(CL)
 figure
+subplot(2,1,1)
+pzmap(CL)
+subplot(2,1,2)
 rlocus(Gol)
+
 figure
-pzplot(C)
-figure
-pzplot(Gol)
+step(CL)
+
+%figure
+% %step(CL)
+% figure
+% rlocus(Gol)
+% figure
+% pzplot(C)
+% figure
+% pzplot(Gol)
