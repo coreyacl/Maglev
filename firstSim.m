58bc6f306727d7026f9516ea921986b5d5501120
Eamon <eamon@piratecat.com>
2020-05-07 23:36:39 -0400

    Tuning

--- firstSim.m
+++ firstSim.m
@@ -6,13 +6,13 @@
 %https://www.daycounter.com/Calculators/Magnets/Solenoid-Force-Calculator.phtml
 %based off of https://media.digikey.com/pdf/Data%20Sheets/Adafruit%20PDFs/3875_Web.pdf
 mu = pi*4e-7;   % permeability of space
-q1 = .01;       % Magnetic force of small magnet in Teslas
+q1 = .01e-3;       % Magnetic force of small magnet in Teslas
 N = 4000;       % # of turns in solenoid
 A = 60;         % Area of solenoid in mm^2
 R = 2.41;       % Internal resistance of solenoid (5V/.6A) in Ohms
 g = -9.81;      % Acceleration due to gravity
 l = 112;
-L = 30e-3;
+L = 10e-3;
 
 %The setpoints for the linerization
 %First we chose our 'b' setpoint for voltage
@@ -29,31 +29,30 @@ G = tf([Z/a^2],[1 0 Z*b/a^3]);
 %% Closed Loop
 close all
 
-k = 40;
-kd = 1;
-kp = 200;
-ki = 10000;
+kd = 69;
+kp = 963;
+ki = 2899;
 H = 1;
 Ac = tf([1],[L R])
-C = tf([kd,kp,ki],[1,0])
-C = C*k;
+C = pid(kp,ki,kd)
 M = 0.03;
+nu = tf([1 50],[1])
+Pi = tf([50],[1 0])
 
-nu = tf([1,200],[1])
+sys = G*Ac
 
 % Visualize system
 % open loop
-Gol = C*G*H*Ac*nu;
+Gol = C*G*H*Ac;
 
 % close loop
-CL = (C*G*Ac*nu- 0.03)/(1+C*G*H*Ac*nu- 0.03) ;
+CL = (C*G*Ac - 0.03)/(1+Gol- 0.03) ;
 
 figure
 subplot(2,1,1)
-pzmap(CL)
+pzmap(G)
 subplot(2,1,2)
 rlocus(Gol)
-
 figure
 step(CL)
 
